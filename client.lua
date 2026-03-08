4d258eebdeabd6eced8845f21f8effb648a7e18b
AgentCape <83658166+AgentCape@users.noreply.github.com>
2026-03-08 15:41:46 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,17 +1,34 @@
-RegisterNetEvent("snus:use")
-AddEventHandler("snus:use", function()
+local usingSnus = false
+
+RegisterNetEvent("snus:client:use", function()
+
+    if usingSnus then
+        return
+    end
+
+    usingSnus = true
 
     local ped = PlayerPedId()
 
-    -- animation
     RequestAnimDict("mp_player_inteat@burger")
     while not HasAnimDictLoaded("mp_player_inteat@burger") do
         Wait(10)
     end
 
-    TaskPlayAnim(ped, "mp_player_inteat@burger", "mp_player_int_eat_burger", 8.0, -8.0, 3000, 49, 0, false, false, false)
+    TaskPlayAnim(
+        ped,
+        "mp_player_inteat@burger",
+        "mp_player_int_eat_burger",
+        8.0,
+        -8.0,
+        3000,
+        49,
+        0,
+        false,
+        false,
+        false
+    )
 
-    -- progressbar
     local duration = 3000
     local start = GetGameTimer()
 
@@ -19,23 +36,28 @@ AddEventHandler("snus:use", function()
         Wait(0)
 
         local progress = (GetGameTimer() - start) / duration
-        DrawRect(0.5, 0.9, 0.2, 0.03, 0, 0, 0, 150)
-        DrawRect(0.4 + (progress * 0.1), 0.9, progress * 0.2, 0.03, 0, 200, 0, 200)
+
+        DrawRect(0.5, 0.92, 0.2, 0.035, 0, 0, 0, 180)
+        DrawRect(0.4 + (progress * 0.1), 0.92, progress * 0.2, 0.03, 0, 180, 0, 220)
     end
 
-    -- nikotin effekt
+    ClearPedTasks(ped)
+
+    -- Nikotin effekt
     StartScreenEffect("DrugsMichaelAliensFight", 5.0, false)
 
     SetRunSprintMultiplierForPlayer(PlayerId(), 1.15)
-
-    Wait(20000)
-
-    SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
-    StopScreenEffect("DrugsMichaelAliensFight")
 
     TriggerEvent("chat:addMessage", {
         color = {0,255,100},
         args = {"Snus", "Nikotin kick!"}
     })
+
+    Wait(20000)
+
+    StopScreenEffect("DrugsMichaelAliensFight")
+    SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
+
+    usingSnus = false
 
 end)
